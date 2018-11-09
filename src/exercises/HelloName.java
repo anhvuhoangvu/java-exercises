6d713054c63948b6379d184f2b6ed308fce7453b
Anh Vu <Anhvuhoangvu@gmail.com>
2018-11-08 18:54:16 -0600

    up to studio 2 done

--- src/exercises/HelloName.java
+++ src/exercises/HelloName.java
@@ -1,11 +1,7 @@
-// Exercise #1:
-// Input/output: Modify your "Hello, World" program to prompt the user for their name, and greet them by name.
-
 package exercises;
-
 import java.util.Scanner;
 
-public class Hello {
+public class HelloName {
 
     public static void main(String[] args) {
         //write code you want to execute here
